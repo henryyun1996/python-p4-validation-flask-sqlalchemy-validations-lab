5b5bc3a75a7a4759d254c566ffc14d8c92b89f0f
Henry Yun <henrysungyun@gmail.com>
2023-04-05 18:35:51 -0700

    finish

--- server/models.py
+++ server/models.py
@@ -3,7 +3,38 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class Author(db.Model):
-    pass
+    __tablename__ = "authors"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    phone_number = db.Column(db.Integer,
+                            db.CheckConstraint('len(phone_number) = 10'),
+                            nullable=False)
+
+    def __repr__(self):
+        return f'<Author: {self.name} | Phone Number: {self.phone_number}>'
+    
+    @validates('name')
+    def validate_name(self, key, name):
+        if name == '':
+            raise ValueError("You must enter a name.")
+        return name
 
 class Post(db.Model):
-    pass
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String,
+                        db.CheckConstraint('len(content) >= 250'),
+                        nullable=False)
+    summary = db.Column(db.String,
+                        db.CheckConstraint('len(summary) < 250'),
+                        nullable=False)
+    category = db.Column(db.String)
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category != 'Fiction' and category != 'Non-fiction':
+            raise ValueError("Category must be either Fiction or Non-Fiction.")
+        return category
